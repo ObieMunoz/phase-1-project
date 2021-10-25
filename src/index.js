8391e00c95d7c450ee98cc63acb7b4cf67a89b08
Obie Munoz <obiemunozjr@gmail.com>
2021-10-25 12:38:33 -0400

    basic combat functional

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const starterBox = document.querySelector("#starter-choices")
 const startScreen = document.querySelector("#start-screen")
 const playerBox = document.querySelector("#fs-player")
 const computerBox = document.querySelector("#fs-computer")
+const fightScreen = document.querySelector("#fight-screen")
 
 let playerHero;
 let computerHero;
+let combatInProgress;
 
 async function retrieveHero(num) {
     for (let i = 0; i < num; i++) {
@@ -15,6 +17,16 @@ async function retrieveHero(num) {
         const resolved = await promise.json()
         const heroString = await resolved.contents;
         const heroObject = await JSON.parse(heroString);
+
+        if (heroObject.powerstats.strength === "null") heroObject.powerstats.strength = 25;
+        if (heroObject.powerstats.intelligence === "null") heroObject.powerstats.intelligence = 25;
+        if (heroObject.powerstats.combat === "null") heroObject.powerstats.combat = 25;
+        if (heroObject.powerstats.power === "null") heroObject.powerstats.power = 25;
+        if (heroObject.powerstats.speed === "null") heroObject.powerstats.speed = 25;
+        
+        heroObject.health = heroObject.powerstats.strength * 100;
+        heroObject.timeToHit = (100 - heroObject.powerstats.speed) * 10;
+        heroObject.damagePerHit = parseInt(heroObject.powerstats.strength) * 2 + parseInt(heroObject.powerstats.combat) * 3 + parseInt(heroObject.powerstats.power)
         heroes.push(heroObject)
         console.log(heroObject)
     }
@@ -22,19 +34,24 @@ async function retrieveHero(num) {
 }
 
 function displayChoices(heroes) {
-    for(let i = 0; i < heroes.length; i++) {
+    starterBox.replaceChildren();
+    for (let i = 0; i < heroes.length; i++) {
         let newHeroObject = heroes[i];
         newHero = document.createElement("img")
         newHero.width = 200;
         newHero.src = heroes[i].image.url;
         newHero.className = 'hero'
         newHero.style.borderRadius = "50%"
-        newHero.onerror = function() {
-            this.remove()
-            retrieveHero(1)
-        }
         newHero.addEventListener("click", (e) => selectHero(e, newHeroObject, i))
         starterBox.append(newHero)
+        newHero.onerror = function () {
+            this.remove()
+            heroes.splice(i, 1)
+            if (heroes.length < 5) {
+                retrieveHero(5 - heroes.length)
+                console.log(`Retrieving ${5 - heroes.length} new heroes`)
+            }
+        }
     }
 }
 
@@ -53,8 +70,41 @@ function setupFight() {
     startScreen.style.display = "none"
     playerBox.style.display = "inline-block"
     computerBox.style.display = "inline-block"
-    playerBox.innerHTML = `<h1>PLAYER</h1><br><h2>${playerHero.name}</h2><img src="${playerHero.image.url}" alt="${playerHero.name}">`
+    playerBox.innerHTML = `<h1>PLAYER</h1><br><h2>${playerHero.name}<span id="playerHealthSpan"></span></h2><img src="${playerHero.image.url}" alt="${playerHero.name}">`
     computerBox.innerHTML = `<h1>COMPUTER</h2><br><h2>${computerHero.name}</h2><img src="${computerHero.image.url}" alt="${computerHero.name}">`
+    fightScreen.innerHTML = `<button id="fight-btn">Fight!</button><br>` + fightScreen.innerHTML;
+    const fightBtn = document.querySelector("#fight-btn")
+    fightBtn.addEventListener("click", combat)
+}
+
+function combat() {
+    combatInProgress = true;
+    playerFighting = setInterval(playerAttack, playerHero.timeToHit)
+    computerFighting = setInterval(computerAttack, computerHero.timeToHit)
+}
+
+function playerAttack() {
+    computerHero.health -= playerHero.damagePerHit;
+    console.log(`${playerHero.name} hits ${computerHero.name}!`)
+    console.log(`${computerHero.name} has ${computerHero.health} health left!`)
+    if (computerHero.health <= 0 || playerHero.health <= 0) combatInProgress = false;
+    if (!combatInProgress) {
+        clearInterval(playerFighting)
+        clearInterval(computerFighting)
+        playerHero.health > computerHero.health ? console.log("Player wins!") : console.log("Computer wins!")
+    }
+}
+
+function computerAttack() {
+    playerHero.health -= computerHero.damagePerHit
+    console.log(`${computerHero.name} hits ${playerHero.name}!`)
+    console.log(`${playerHero.name} has ${playerHero.health} health left!`)
+    if (computerHero.health <= 0 || playerHero.health <= 0) combatInProgress = false;
+    if (!combatInProgress) {
+        clearInterval(playerFighting)
+        clearInterval(computerFighting)
+        playerHero.health > computerHero.health ? console.log("Player wins!") : console.log("Computer wins!")
+    }
 }
 
 retrieveHero(5);
